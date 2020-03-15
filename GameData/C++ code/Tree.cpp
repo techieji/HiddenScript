43da64d48e76f5ff126621b52eeb5d7c9947d6f4
techieji <drpradhyum2016@outlook.com>
2020-03-15 13:42:11 -0400

    Implemented FindExtension

--- GameData/C++ code/Tree.cpp
+++ GameData/C++ code/Tree.cpp
@@ -12,6 +12,7 @@ class Tree {
     vector<fs::directory_entry> files;
 public:
     Tree(fs::path startpath);
+    vector<fs::path> FindExtension(string ex);
 };
 
 Tree::Tree(fs::path startpath) {
@@ -23,4 +24,18 @@ Tree::Tree(fs::path startpath) {
             files.push_back(file.path);
         }
     }
+}
+
+vector<fs::path> Tree::FindExtension(string ex) {
+    vector<fs::path> returnval;
+    for (const auto & file : files) {
+        if (file.path.extension().string() == ex) {
+            returnval.push_back(file.path);
+        }
+    }
+    for (auto & dir : branches) {
+        vector<fs::path> retvec = dir.FindExtension(ex);
+        returnval.insert(returnval.end(), retvec.begin(), retvec.end());
+    }
+    return returnval;
 }
